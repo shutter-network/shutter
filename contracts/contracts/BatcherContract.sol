473c7b5cd601984085affd851703775f7677054e
Jannik Luhn <jannik@brainbot.com>
2020-08-11 13:18:21 +0200

    Implement batcher contract

---
+++ contracts/contracts/BatcherContract.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "./ConfigContract.sol";
+import "./Ownable.sol";
+
+contract BatcherContract is Ownable {
+
+    enum TransactionType {
+        Cipher,
+        Plain
+    }
+
+    event TransactionAdded (
+        uint256 batchIndex,
+        TransactionType transactionType,
+        bytes transaction,
+        bytes32 batchHash
+    );
+
+    ConfigContract public configContract;
+    mapping(uint256 => uint256) public batchSizes;
+    mapping(uint256 => mapping(TransactionType => bytes32)) public batchHashes;
+
+    uint256 public minFee;
+
+    constructor(address _configContractAddress) {
+        configContract = ConfigContract(_configContractAddress);
+    }
+
+    function addTransaction(uint256 _batchIndex, TransactionType _type, bytes calldata _transaction) external payable {
+        BatchConfig memory config = configContract.getConfig(_batchIndex);
+        assert(block.number >= config.startBlockNumber);
+
+        require(config.active);
+
+        // TODO: over/underflows?
+        uint256 _relativeBatchIndex = _batchIndex - config.startBatchIndex;
+        require(block.number < config.startBlockNumber + config.batchSpan * (_relativeBatchIndex + 1));
+
+        require(_transaction.length > 0);
+        require(_transaction.length <= config.transactionSizeLimit);
+
+        require(batchSizes[_batchIndex] + _transaction.length <= config.batchSizeLimit);
+
+        require(msg.value >= minFee);
+
+        bytes memory _batchHashPreimage = abi.encodePacked(batchHashes[_batchIndex][_type], _transaction);
+        bytes32 _newBatchHash = keccak256(_batchHashPreimage);
+        batchHashes[_batchIndex][_type] = _newBatchHash;
+        batchSizes[_batchIndex] += _transaction.length;
+
+        emit TransactionAdded(
+            _batchIndex, _type, _transaction, _newBatchHash
+        );
+        (bool success,) = config.feeReceiver.call{value: msg.value}("");  // TODO: check if reentrancy could be an issue
+        require(success);
+    }
+
+    function setMinFee(uint256 _minFee) external onlyOwner {
+        minFee = _minFee;
+    }
+}
---
+++ contracts/tests/test_batcher_contract.py
@@ -0,0 +1,236 @@
+from typing import Any
+from typing import Sequence
+
+import brownie
+from brownie.network.state import Chain
+from eth_typing import Address
+from eth_utils import encode_hex
+from eth_utils import keccak
+from eth_utils import to_canonical_address
+from tests.contract_helpers import mine_until
+from tests.contract_helpers import schedule_config
+from tests.factories import make_batch_config
+
+
+def test_add_tx_fails_if_not_active(
+    batcher_contract: Any,
+    config_contract: Any,
+    chain: Chain,
+    config_change_heads_up_blocks: int,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        active=False,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number, chain)
+
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"\x00")
+
+
+def test_add_tx_checks_batching_period_end(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number + 98, chain)
+
+    batcher_contract.addTransaction(0, 0, b"\x00")
+    assert chain.height == config.start_block_number + 99
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"\x00")
+    batcher_contract.addTransaction(1, 0, b"\x00")
+
+
+def test_add_tx_checks_tx_size(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+        transaction_size_limit=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"")
+
+    batcher_contract.addTransaction(0, 0, b"\x00" * 100)
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"\x00" * 101)
+
+
+def test_add_tx_checks_batch_size(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+        transaction_size_limit=100,
+        batch_size_limit=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    for _ in range(10):
+        batcher_contract.addTransaction(0, 0, b"\x00" * 10)
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"\x00")
+
+
+def test_add_tx_checks_fee(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    batcher_contract.setMinFee(100, {"from": owner})
+    with brownie.reverts():
+        batcher_contract.addTransaction(0, 0, b"\x00", {"value": 99})
+    batcher_contract.addTransaction(0, 0, b"\x00", {"value": 100})
+
+
+def test_add_tx_updates_hash_chain(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    for tx_type in [0, 1]:
+        assert bytes(batcher_contract.batchHashes(0, tx_type)) == b"\x00" * 32
+        batcher_contract.addTransaction(0, tx_type, b"\x11")
+        assert bytes(batcher_contract.batchHashes(0, tx_type)) == keccak(b"\x00" * 32 + b"\x11")
+        batcher_contract.addTransaction(0, tx_type, b"\x22")
+        assert bytes(batcher_contract.batchHashes(0, tx_type)) == keccak(
+            keccak(b"\x00" * 32 + b"\x11") + b"\x22"
+        )
+
+
+def test_add_tx_updates_batch_size(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    assert batcher_contract.batchSizes(0) == 0
+    batcher_contract.addTransaction(0, 0, b"\x00" * 3)
+    assert batcher_contract.batchSizes(0) == 3
+    batcher_contract.addTransaction(0, 1, b"\x00" * 5)
+    assert batcher_contract.batchSizes(0) == 8
+
+
+def test_add_tx_emits_event(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+) -> None:
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    tx = batcher_contract.addTransaction(0, 0, b"\x11")
+    assert len(tx.events) == 1
+    assert tx.events[0] == {
+        "batchIndex": 0,
+        "transactionType": 0,
+        "transaction": "0x11",
+        "batchHash": encode_hex(keccak(b"\x00" * 32 + b"\x11")),
+    }
+
+    tx = batcher_contract.addTransaction(2, 1, b"\x22")
+    assert len(tx.events) == 1
+    assert tx.events[0] == {
+        "batchIndex": 2,
+        "transactionType": 1,
+        "transaction": "0x22",
+        "batchHash": encode_hex(keccak(b"\x00" * 32 + b"\x22")),
+    }
+
+
+def test_add_tx_pays_fee(
+    batcher_contract: Any,
+    config_contract: Any,
+    config_change_heads_up_blocks: int,
+    chain: Chain,
+    owner: Address,
+    accounts: Sequence[Address],
+) -> None:
+    fee_receiver = accounts[1]
+    config = make_batch_config(
+        start_batch_index=0,
+        start_block_number=chain.height + config_change_heads_up_blocks + 20,
+        batch_span=100,
+        fee_receiver=to_canonical_address(fee_receiver.address),
+    )
+    schedule_config(config_contract, config, owner=owner)
+    mine_until(config.start_block_number - 1, chain)
+
+    initial_balance = fee_receiver.balance()
+    batcher_contract.addTransaction(0, 0, b"\x00", {"value": 150})
+    assert fee_receiver.balance() == initial_balance + 150
+
+
+def test_set_fee(batcher_contract: Any, owner: Address) -> None:
+    assert batcher_contract.minFee() == 0
+    batcher_contract.setMinFee(100, {"from": owner})
+    assert batcher_contract.minFee() == 100
+
+
+def test_non_owner_cannot_set_fee(batcher_contract: Any, non_owner: Address) -> None:
+    with brownie.reverts():
+        batcher_contract.setMinFee(100, {"from": non_owner})
