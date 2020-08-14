937f67e53fdbc2020accd89d1e8343a0d261d807
Ralf Schmitt <ralf@brainbot.com>
2020-08-14 12:08:33 +0200

    Add short test that demonstrates how to sign messages

--- shuttermint/go.mod
+++ shuttermint/go.mod
@@ -3,11 +3,12 @@ module github.com/brainbot-com/shutter/shuttermint
 go 1.14
 
 require (
+	github.com/ethereum/go-ethereum v1.9.19
 	github.com/golang/protobuf v1.4.0
 	github.com/google/go-cmp v0.5.0 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/spf13/viper v1.6.3
 	github.com/tendermint/tendermint v0.33.8
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	google.golang.org/protobuf v1.21.0
 )
---
+++ shuttermint/sandbox/signing_test.go
@@ -0,0 +1,46 @@
+// Sandbox just contains example code how to use certain libraries
+package sandbox
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+	"golang.org/x/crypto/sha3"
+)
+
+// TestSigning shows how to sign messages and check signatures
+func TestSigning(t *testing.T) {
+	// pk, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf("fatal: %s", err)
+	}
+
+	msg := []byte("message to be signed")
+
+	hash := sha3.New256()
+	hash.Write(msg)
+	h := hash.Sum(nil)
+
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	t.Logf("generated key for address %s", address.Hex())
+	signature, err := crypto.Sign(h, privateKey)
+	if err != nil {
+		t.Fatalf("error %s %s", signature, err)
+	} else {
+		t.Logf("signature: %s", hexutil.Encode(signature))
+	}
+
+	// Now check the signature
+
+	pubkey, err := crypto.SigToPub(h, signature)
+	if err != nil {
+		t.Fatalf("error %s", err)
+	}
+	signer_address := crypto.PubkeyToAddress(*pubkey)
+	t.Logf("signer %s", signer_address.Hex())
+	if signer_address != address {
+		t.Fatalf("addresses to not match")
+	}
+}
