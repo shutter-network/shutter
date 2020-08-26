9eaefe93dbedea671218c9c721376024e93d28ac
Ralf Schmitt <ralf@brainbot.com>
2020-08-26 08:48:11 +0200

    Make sure addConfig only accepts increasing StartBatchIndex's

--- shuttermint/app/app_test.go
+++ shuttermint/app/app_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestNewShutterApp(t *testing.T) {
 	app := NewShutterApp()
-	require.Equal(t, len(app.Configs), 1)
-	require.Equal(t, app.Configs[0], &BatchConfig{})
+	require.Equal(t, len(app.Configs), 1, "Configs should contain exactly one guard element")
+	require.Equal(t, app.Configs[0], &BatchConfig{}, "Bad guard element")
 }
 
 func TestGetBatch(t *testing.T) {
@@ -31,4 +31,17 @@ func TestGetBatch(t *testing.T) {
 	require.Equal(t, app.getBatch(199).Config.Threshhold, uint32(1))
 	require.Equal(t, app.getBatch(200).Config.Threshhold, uint32(2))
 	require.Equal(t, app.getBatch(1000).Config.Threshhold, uint32(3))
+}
+
+func TestAddConfig(t *testing.T) {
+	app := NewShutterApp()
+
+	err := app.addConfig(BatchConfig{StartBatchIndex: 100, Threshhold: 1})
+	require.Nil(t, err)
+
+	err = app.addConfig(BatchConfig{StartBatchIndex: 50, Threshhold: 1})
+	require.NotNil(t, err, "Expected error, StartBatchIndex must increase")
+
+	err = app.addConfig(BatchConfig{StartBatchIndex: 100, Threshhold: 1})
+	require.NotNil(t, err, "Expected error, StartBatchIndex must increase")
 }
