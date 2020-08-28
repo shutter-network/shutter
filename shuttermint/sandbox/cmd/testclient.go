1151c2d5ca932ac2b0e3a429f69c4c2c272a14dc
Ralf Schmitt <ralf@brainbot.com>
2020-08-28 17:59:04 +0200

    Use the tendermint client library

--- shuttermint/sandbox/cmd/testclient.go
+++ shuttermint/sandbox/cmd/testclient.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
 	"encoding/base64"
 	"fmt"
 
 	"github.com/brainbot-com/shutter/shuttermint/shmsg"
-	"github.com/brainbot-com/shutter/shuttermint/smclient"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/tendermint/tendermint/rpc/client/http"
+	"github.com/tendermint/tendermint/types"
 )
 
 func makeMessage() *shmsg.Message {
@@ -22,16 +22,26 @@ func makeMessage() *shmsg.Message {
 }
 
 func main() {
-	cl, err := smclient.Dial("http://localhost:26657")
+	cl, err := http.New("http://localhost:26657", "/websocket")
 	if err != nil {
 		panic(err)
 	}
+	// if !cl.IsRunning() {
+	//	panic("tendermint not running")
+	// }
+
 	fmt.Println("got a client:", cl)
-	st, err := cl.GetStatus(context.Background())
+	st, err := cl.Status()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(st))
+	fmt.Printf("Status: %+v\n", st)
+
+	// st, err := cl.GetStatus(context.Background())
+	// if err != nil {
+	//	panic(err)
+	// }
+	// fmt.Println(string(st))
 
 	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
 	if err != nil {
@@ -42,8 +52,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var tx types.Tx
+	tx = types.Tx(base64.RawURLEncoding.EncodeToString(signedMessage))
+	res, err := cl.BroadcastTxCommit(tx)
+
 	fmt.Println("Msg:", base64.RawURLEncoding.EncodeToString(signedMessage))
-	res, err := cl.BroadcastTXCommit(context.Background(), signedMessage)
+
+	// res, err := cl.BroadcastTXCommit(context.Background(), signedMessage)
 	if err != nil {
 		panic(err)
 	}
