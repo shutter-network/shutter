f8abbac6ff84446ef44ee229ceb65ceb6540b54b
Ralf Schmitt <ralf@brainbot.com>
2020-08-31 21:54:45 +0200

    Configure logging

--- shuttermint/keyper/cmd/main.go
+++ shuttermint/keyper/cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/brainbot-com/shutter/shuttermint/keyper"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
 	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
 	if err != nil {
 		panic(err)
