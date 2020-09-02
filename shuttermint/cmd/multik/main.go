94545cd39c572378483ba657a96a863620a658f7
Ralf Schmitt <ralf@brainbot.com>
2020-09-02 09:03:40 +0200

    Build all executables with 'make build' inside the bin directory
    Also provide the output of 'git describe' as main.version.

--- shuttermint/cmd/multik/main.go
+++ shuttermint/cmd/multik/main.go
@@ -13,8 +13,11 @@ import (
 	"github.com/tendermint/tendermint/rpc/client/http"
 )
 
+var version string = "(unknown)"
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	log.Printf("Starting multik version %s", version)
 	shuttermintURL := "http://localhost:26657"
 	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
 	if err != nil {
