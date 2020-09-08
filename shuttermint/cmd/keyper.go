8c3d8f8729016cc98b17bd4f84dc1a8db7d10915
Ralf Schmitt <ralf@brainbot.com>
2020-09-08 12:54:41 +0200

    Configure keyper via viper
    At the moment the configuration file looks like:
    ,----[ keyper.toml ]
    | ShuttermintURL = "http://localhost:26657"
    | SigningKey = "488cb263375c044987eb865c461d8df32580747a07b2d5bd0623a938e0db32cf"
    `----
    
    We read the config from ~/.config/shutter/keyper.toml or the filename
    given with --config.
    
    Additionally the values can also be configured via environment
    variables KEYPER_SIGNINGKEY and KEYPER_SHUTTERMINTURL.

--- shuttermint/cmd/keyper.go
+++ shuttermint/cmd/keyper.go
@@ -1,24 +1,26 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/brainbot-com/shutter/shuttermint/keyper"
 	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+type KeyperConfig struct {
+	ShuttermintURL string
+	SigningKey     string
+}
 
 // keyperCmd represents the keyper command
 var keyperCmd = &cobra.Command{
 	Use:   "keyper",
 	Short: "Run a shutter keyper",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		keyperMain()
 	},
@@ -26,17 +28,62 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(keyperCmd)
+	keyperCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file")
+}
+
+func readKeyperConfig() (KeyperConfig, error) {
+	viper.SetEnvPrefix("KEYPER")
+	viper.BindEnv("ShuttermintURL")
+	viper.BindEnv("SigningKey")
+	viper.SetDefault("ShuttermintURL", "http://localhost:26657")
+
+	var err error
+	kc := KeyperConfig{}
+
+	viper.AddConfigPath("$HOME/.config/shutter")
+	viper.SetConfigName("keyper")
+	viper.SetConfigType("toml")
+
+	if cfgFile == "" {
+		err = viper.ReadInConfig()
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found
+		} else {
+			return kc, err // Config file was found but another error was produced
+		}
+	} else {
+		f, err := os.Open(cfgFile)
+		if err != nil {
+			return kc, err
+		}
+		defer f.Close()
+
+		err = viper.ReadConfig(f)
+		if err != nil {
+			return kc, err
+		}
+	}
+
+	err = viper.Unmarshal(&kc)
+	return kc, err
 }
 
 func keyperMain() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
-	log.Printf("Starting keyper version %s", version)
-
-	privateKey, err := crypto.HexToECDSA("fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19")
+	kc, err := readKeyperConfig()
 	if err != nil {
 		panic(err)
 	}
-	k := keyper.NewKeyper(privateKey, "http://localhost:26657")
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+
+	privateKey, err := crypto.HexToECDSA(kc.SigningKey)
+	if err != nil {
+		panic(fmt.Errorf("Bad signing key: %s", err))
+	}
+
+	addr := crypto.PubkeyToAddress(privateKey.PublicKey).Hex()
+	log.Printf("Starting keyper version %s with signing key %s, using %s", version, addr, kc.ShuttermintURL)
+	k := keyper.NewKeyper(privateKey, kc.ShuttermintURL)
 	err = k.Run()
 	if err != nil {
 		panic(err)
