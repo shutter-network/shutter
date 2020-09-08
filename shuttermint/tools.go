3b77aee36f27e7c3f20c3fd40bea81dc9a1fe8be
Ralf Schmitt <ralf@brainbot.com>
2020-09-08 22:32:05 +0200

    Install cobra
    `get get` will update the module to be installed, which is why we now
    use `go install` instead.

--- shuttermint/tools.go
+++ shuttermint/tools.go
@@ -8,5 +8,6 @@ package tools
 import (
 	_ "github.com/ethereum/go-ethereum/cmd/abigen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/spf13/cobra/cobra"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
