9eb8a85949048d457bab4b012a02a4b29f6855dc
Ralf Schmitt <ralf@brainbot.com>
2020-09-10 13:06:42 +0200

    Add a short script to deploy contracts to ganache

---
+++ shuttermint/sandbox/deploy/deploy.go
@@ -0,0 +1,65 @@
+// Deploy the ConfigContract and KeyBroadcastContract to ganache. This uses a hard-coded private
+// key that available when ganache is started with the -d flag.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/brainbot-com/shutter/shuttermint/contract"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This is the last key used by 'ganache-cli -d'
+	privateKey, err := crypto.HexToECDSA("b0057716d5917badaf911b193b12b910811c1497b5bada8d7711f758981c3773")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fromAddress := crypto.PubkeyToAddress(privateKey.PublicKey)
+
+	newAuth := func() *bind.TransactOpts {
+		nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		gasPrice, err := client.SuggestGasPrice(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		auth := bind.NewKeyedTransactor(privateKey)
+		auth.Nonce = big.NewInt(int64(nonce))
+		auth.Value = big.NewInt(0)      // in wei
+		auth.GasLimit = uint64(5000000) // in units
+		auth.GasPrice = gasPrice
+		return auth
+	}
+
+	configAddress, tx, _, err := contract.DeployConfigContract(newAuth(), client, big.NewInt(5))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("ConfigContract address:", configAddress.Hex())
+	fmt.Println(tx.Hash().Hex())
+
+	address, tx, _, err := contract.DeployKeyBroadcastContract(newAuth(), client, configAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nDeployKeyBroadcastContract address:", address.Hex())
+	fmt.Println(tx.Hash().Hex())
+}
