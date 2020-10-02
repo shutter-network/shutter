acf27c5446293fbc458132f2d144ce27e324db16
Jannik Luhn <jannik@brainbot.com>
2020-10-02 16:21:49 +0200

    Don't vote for start in multik
    this is already done by the bootstrap script. Later configs are started
    automatically by the keypers.

--- shuttermint/cmd/multik.go
+++ shuttermint/cmd/multik.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"context"
 	"crypto/ecdsa"
 	"crypto/ed25519"
 	"log"
@@ -9,12 +8,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/spf13/cobra"
-	"github.com/tendermint/tendermint/rpc/client"
-	"github.com/tendermint/tendermint/rpc/client/http"
 
-	"github.com/brainbot-com/shutter/shuttermint/contract"
 	"github.com/brainbot-com/shutter/shuttermint/keyper"
 	"github.com/brainbot-com/shutter/shuttermint/sandbox"
 )
@@ -42,8 +37,6 @@ func multikMain() {
 		KeyBroadcastingContractAddress: common.HexToAddress("0xFA33c8EF8b5c4f3003361c876a298D1DB61ccA4e"),
 	}
 
-	privateKey := sandbox.GanacheKey(sandbox.NumGanacheKeys() - 1)
-
 	var signingKeys [3]*ecdsa.PrivateKey
 	var validatorKeys [3]ed25519.PrivateKey
 	var keypers [3]common.Address
@@ -57,32 +50,6 @@ func multikMain() {
 		validatorKeys[i] = ed25519.NewKeyFromSeed(validatorSeed)
 	}
 
-	ethcl, err := ethclient.Dial(baseConfig.EthereumURL)
-	configContract, err := contract.NewConfigContract(baseConfig.ConfigContractAddress, ethcl)
-	header, err := ethcl.HeaderByNumber(context.Background(), nil)
-	if err != nil {
-		panic(err)
-	}
-	headBlockNumber := header.Number.Uint64()
-
-	startBatchIndex, err := configContract.NextBatchIndex(headBlockNumber)
-	if err != nil {
-		panic(err)
-	}
-
-	var shmcl client.Client
-	shmcl, err = http.New(baseConfig.ShuttermintURL, "/websocket")
-	if err != nil {
-		panic(err)
-	}
-
-	ms := keyper.NewMessageSender(shmcl, privateKey)
-	bc := keyper.NewBatchConfig(startBatchIndex, keypers[:], 2, 999999)
-	err = ms.SendMessage(bc)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Send new BatchConfig (start batch index %d)", startBatchIndex)
 	for i := 0; i < 3; i++ {
 		go func(signingKey *ecdsa.PrivateKey, validatorKey ed25519.PrivateKey) {
 			config := baseConfig
@@ -90,7 +57,7 @@ func multikMain() {
 			config.ValidatorKey = validatorKey
 
 			kpr := keyper.NewKeyper(config)
-			err = kpr.Run()
+			err := kpr.Run()
 			if err != nil {
 				panic(err)
 			}
