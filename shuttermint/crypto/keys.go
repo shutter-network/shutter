2a5081af35ede7086bc3028d611fc5dac70e7f52
Jannik Luhn <jannik@brainbot.com>
2020-11-09 14:48:30 +0100

    Implement key generation and verification crypto

---
+++ shuttermint/crypto/keys.go
@@ -0,0 +1,135 @@
+package crypto
+
+import (
+	"fmt"
+	"math/big"
+
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+)
+
+// EonSKShare represents a share of the eon secret key.
+type EonSKShare big.Int
+
+// EonPKShare represents a share of the eon public key.
+type EonPKShare bn256.G2
+
+// EonPK represents the combined eon public key.
+type EonPK bn256.G2
+
+// EpochID is the identifier of an epoch.
+type EpochID bn256.G1
+
+// EpochSKShare represents a keyper's share of the epoch sk key.
+type EpochSKShare bn256.G1
+
+// EpochSK represents an epoch secret key.
+type EpochSK bn256.G1
+
+// ComputeEonSKShare computes the a keyper's sk share from the set of poly evals received from the
+// other keypers.
+func ComputeEonSKShare(polyEvals []*big.Int) *EonSKShare {
+	res := big.NewInt(0)
+	for _, vi := range polyEvals {
+		res.Add(res, vi)
+		res.Mod(res, bn256.Order)
+	}
+	share := EonSKShare(*res)
+	return &share
+}
+
+// ComputeEonPKShare computes the eon public key share of the given keyper.
+func ComputeEonPKShare(keyperIndex int, gammas []*Gammas) *EonPKShare {
+	g2 := new(bn256.G2).Set(zeroG2)
+	keyperX := KeyperX(keyperIndex)
+	for _, gs := range gammas {
+		mu := gs.Mu(keyperX)
+		g2 = g2.Add(g2, mu)
+	}
+	epk := EonPKShare(*g2)
+	return &epk
+}
+
+// ComputeEonPK computes the combined eon public key from the set of eon public key shares.
+func ComputeEonPK(pkShares []*EonPKShare) *EonPK {
+	g2 := new(bn256.G2).Set(zeroG2)
+	for _, share := range pkShares {
+		g2 = g2.Add(g2, (*bn256.G2)(share))
+	}
+	epk := EonPK(*g2)
+	return &epk
+}
+
+// ComputeEpochSKShare computes a keyper's epoch sk share.
+func ComputeEpochSKShare(eonSKShare *EonSKShare, epochID *EpochID) *EpochSKShare {
+	g1 := new(bn256.G1).ScalarMult((*bn256.G1)(epochID), (*big.Int)(eonSKShare))
+	epochSKShare := EpochSKShare(*g1)
+	return &epochSKShare
+}
+
+// ComputeEpochID computes the id of the given epoch.
+func ComputeEpochID(epochIndex uint64) *EpochID {
+	epochIndexBig := new(big.Int).SetUint64(epochIndex)
+	id := EpochID(*new(bn256.G1).ScalarBaseMult(epochIndexBig))
+	return &id
+}
+
+// ComputeEpochSK computes the epoch secret key from a set of shares.
+func ComputeEpochSK(keyperIndices []int, epochSKShares []*EpochSKShare) (*EpochSK, error) {
+	if len(keyperIndices) != len(epochSKShares) {
+		return nil, fmt.Errorf("got %d keyper indices, but %d secret shares", len(keyperIndices), len(epochSKShares))
+	}
+
+	skG1 := new(bn256.G1).Set(zeroG1)
+	for i := 0; i < len(keyperIndices); i++ {
+		keyperIndex := keyperIndices[i]
+		share := epochSKShares[i]
+
+		lambda := lagrangeCoefficient(keyperIndex, keyperIndices)
+		qTimesLambda := new(bn256.G1).ScalarMult((*bn256.G1)(share), lambda)
+		skG1 = skG1.Add(skG1, qTimesLambda)
+	}
+	sk := EpochSK(*skG1)
+	return &sk, nil
+}
+
+// VerifyEpochSKShare checks that an epoch sk share published by a keyper is correct.
+func VerifyEpochSKShare(epochSKShare *EpochSKShare, eonPKShare *EonPKShare, epochID *EpochID) bool {
+	g1s := []*bn256.G1{
+		(*bn256.G1)(epochSKShare),
+		new(bn256.G1).Neg((*bn256.G1)(epochID)),
+	}
+	g2s := []*bn256.G2{
+		new(bn256.G2).ScalarBaseMult(big.NewInt(1)),
+		(*bn256.G2)(eonPKShare),
+	}
+	return bn256.PairingCheck(g1s, g2s)
+}
+
+func lagrangeCoefficientFactor(k int, keyperIndex int) *big.Int {
+	xj := KeyperX(keyperIndex)
+	xk := KeyperX(k)
+	dx := new(big.Int).Sub(xk, xj)
+	dx.Mod(dx, bn256.Order)
+	dxInv := invert(dx)
+	lambdaK := new(big.Int).Mul(xk, dxInv)
+	lambdaK.Mod(lambdaK, bn256.Order)
+	return lambdaK
+}
+
+func lagrangeCoefficient(keyperIndex int, keyperIndices []int) *big.Int {
+	lambda := big.NewInt(1)
+	for _, k := range keyperIndices {
+		if k == keyperIndex {
+			continue
+		}
+		lambdaK := lagrangeCoefficientFactor(k, keyperIndex)
+		lambda.Mul(lambda, lambdaK)
+		lambda.Mod(lambda, bn256.Order)
+	}
+	return lambda
+}
+
+func invert(x *big.Int) *big.Int {
+	orderMinus2 := new(big.Int).Sub(bn256.Order, big.NewInt(2))
+	return new(big.Int).Exp(x, orderMinus2, bn256.Order)
+}
---
+++ shuttermint/crypto/keys_test.go
@@ -0,0 +1,389 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"math/big"
+	"testing"
+
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEonSKShare(t *testing.T) {
+	zeroKey := ComputeEonSKShare([]*big.Int{})
+	require.Zero(t, (*big.Int)(zeroKey).Sign())
+
+	key1 := ComputeEonSKShare([]*big.Int{
+		big.NewInt(10),
+		big.NewInt(20),
+		big.NewInt(30),
+	})
+	require.Zero(t, big.NewInt(60).Cmp((*big.Int)(key1)))
+
+	key2 := ComputeEonSKShare([]*big.Int{
+		bn256.Order,
+		big.NewInt(10),
+		bn256.Order,
+		big.NewInt(20),
+		bn256.Order,
+	})
+	require.Zero(t, big.NewInt(30).Cmp((*big.Int)(key2)))
+}
+
+func TestEonPKShare(t *testing.T) {
+	gammas0 := Gammas{
+		new(bn256.G2).ScalarBaseMult(big.NewInt(1)),
+		new(bn256.G2).ScalarBaseMult(big.NewInt(2)),
+	}
+	gammas1 := Gammas{
+		new(bn256.G2).ScalarBaseMult(big.NewInt(3)),
+		new(bn256.G2).ScalarBaseMult(big.NewInt(4)),
+	}
+	gammas2 := Gammas{
+		new(bn256.G2).ScalarBaseMult(big.NewInt(5)),
+		new(bn256.G2).ScalarBaseMult(big.NewInt(6)),
+	}
+	gammas := []*Gammas{
+		&gammas0,
+		&gammas1,
+		&gammas2,
+	}
+
+	x0 := KeyperX(0)
+	x1 := KeyperX(1)
+	x2 := KeyperX(2)
+
+	mu00 := new(bn256.G2).Add(gammas0[0], new(bn256.G2).ScalarMult(gammas0[1], x0))
+	mu01 := new(bn256.G2).Add(gammas1[0], new(bn256.G2).ScalarMult(gammas1[1], x0))
+	mu02 := new(bn256.G2).Add(gammas2[0], new(bn256.G2).ScalarMult(gammas2[1], x0))
+	mu10 := new(bn256.G2).Add(gammas0[0], new(bn256.G2).ScalarMult(gammas0[1], x1))
+	mu11 := new(bn256.G2).Add(gammas1[0], new(bn256.G2).ScalarMult(gammas1[1], x1))
+	mu12 := new(bn256.G2).Add(gammas2[0], new(bn256.G2).ScalarMult(gammas2[1], x1))
+	mu20 := new(bn256.G2).Add(gammas0[0], new(bn256.G2).ScalarMult(gammas0[1], x2))
+	mu21 := new(bn256.G2).Add(gammas1[0], new(bn256.G2).ScalarMult(gammas1[1], x2))
+	mu22 := new(bn256.G2).Add(gammas2[0], new(bn256.G2).ScalarMult(gammas2[1], x2))
+
+	pks0 := new(bn256.G2).Add(mu00, mu01)
+	pks0.Add(pks0, mu02)
+	pks1 := new(bn256.G2).Add(mu10, mu11)
+	pks1.Add(pks1, mu12)
+	pks2 := new(bn256.G2).Add(mu20, mu21)
+	pks2.Add(pks2, mu22)
+
+	require.True(t, EqualG2(pks0, (*bn256.G2)(ComputeEonPKShare(0, gammas))))
+	require.True(t, EqualG2(pks1, (*bn256.G2)(ComputeEonPKShare(1, gammas))))
+	require.True(t, EqualG2(pks2, (*bn256.G2)(ComputeEonPKShare(2, gammas))))
+}
+
+func TestEonSharesMatch(t *testing.T) {
+	threshold := uint64(2)
+	p1, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(10))
+	require.Nil(t, err)
+	p2, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(20))
+	require.Nil(t, err)
+	p3, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(30))
+	require.Nil(t, err)
+
+	x1 := KeyperX(0)
+	x2 := KeyperX(1)
+	x3 := KeyperX(2)
+
+	gammas := []*Gammas{p1.Gammas(), p2.Gammas(), p3.Gammas()}
+
+	v11 := p1.Eval(x1)
+	v21 := p1.Eval(x2)
+	v31 := p1.Eval(x3)
+	v12 := p2.Eval(x1)
+	v22 := p2.Eval(x2)
+	v32 := p2.Eval(x3)
+	v13 := p3.Eval(x1)
+	v23 := p3.Eval(x2)
+	v33 := p3.Eval(x3)
+
+	esk1 := ComputeEonSKShare([]*big.Int{v11, v12, v13})
+	esk2 := ComputeEonSKShare([]*big.Int{v21, v22, v23})
+	esk3 := ComputeEonSKShare([]*big.Int{v31, v32, v33})
+
+	epk1 := ComputeEonPKShare(0, gammas)
+	epk2 := ComputeEonPKShare(1, gammas)
+	epk3 := ComputeEonPKShare(2, gammas)
+
+	epk1Exp := new(bn256.G2).ScalarBaseMult((*big.Int)(esk1))
+	epk2Exp := new(bn256.G2).ScalarBaseMult((*big.Int)(esk2))
+	epk3Exp := new(bn256.G2).ScalarBaseMult((*big.Int)(esk3))
+
+	require.True(t, EqualG2((*bn256.G2)(epk1), epk1Exp))
+	require.True(t, EqualG2((*bn256.G2)(epk2), epk2Exp))
+	require.True(t, EqualG2((*bn256.G2)(epk3), epk3Exp))
+}
+
+func TestEonPK(t *testing.T) {
+	zeroEPK := ComputeEonPK([]*EonPKShare{})
+	require.True(t, EqualG2((*bn256.G2)(zeroEPK), zeroG2))
+
+	p1 := new(bn256.G2).ScalarBaseMult(big.NewInt(1))
+	p2 := new(bn256.G2).ScalarBaseMult(big.NewInt(2))
+	p3 := new(bn256.G2).ScalarBaseMult(big.NewInt(3))
+	epk1 := EonPKShare(*p1)
+	epk2 := EonPKShare(*p2)
+	epk3 := EonPKShare(*p3)
+
+	k1 := ComputeEonPK([]*EonPKShare{&epk1})
+	require.True(t, EqualG2((*bn256.G2)(k1), p1))
+	k2 := ComputeEonPK([]*EonPKShare{&epk2})
+	require.True(t, EqualG2((*bn256.G2)(k2), p2))
+	k3 := ComputeEonPK([]*EonPKShare{&epk3})
+	require.True(t, EqualG2((*bn256.G2)(k3), p3))
+
+	k12 := ComputeEonPK([]*EonPKShare{&epk1, &epk2})
+	require.True(t, EqualG2((*bn256.G2)(k12), new(bn256.G2).ScalarBaseMult(big.NewInt(3))))
+	k13 := ComputeEonPK([]*EonPKShare{&epk1, &epk3})
+	require.True(t, EqualG2((*bn256.G2)(k13), new(bn256.G2).ScalarBaseMult(big.NewInt(4))))
+	k23 := ComputeEonPK([]*EonPKShare{&epk2, &epk3})
+	require.True(t, EqualG2((*bn256.G2)(k23), new(bn256.G2).ScalarBaseMult(big.NewInt(5))))
+
+	k123 := ComputeEonPK([]*EonPKShare{&epk1, &epk2, &epk3})
+	require.True(t, EqualG2((*bn256.G2)(k123), new(bn256.G2).ScalarBaseMult(big.NewInt(6))))
+}
+
+func TestEonPKMatchesSK(t *testing.T) {
+	threshold := uint64(2)
+	p1, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(10))
+	require.Nil(t, err)
+	p2, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(20))
+	require.Nil(t, err)
+	p3, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(30))
+	require.Nil(t, err)
+
+	esk := big.NewInt(0)
+	for _, p := range []*Polynomial{p1, p2, p3} {
+		for i := 0; i < 3; i++ {
+			esk = esk.Add(esk, p.EvalForKeyper(i))
+		}
+	}
+
+	gammas := []*Gammas{p1.Gammas(), p2.Gammas(), p3.Gammas()}
+	epk1 := ComputeEonPKShare(0, gammas)
+	epk2 := ComputeEonPKShare(1, gammas)
+	epk3 := ComputeEonPKShare(2, gammas)
+
+	epk := ComputeEonPK([]*EonPKShare{epk1, epk2, epk3})
+	epkExp := new(bn256.G2).ScalarBaseMult(esk)
+	require.True(t, EqualG2((*bn256.G2)(epk), epkExp))
+}
+
+func TestInverse(t *testing.T) {
+	testCases := []*big.Int{
+		big.NewInt(1),
+		big.NewInt(2),
+		big.NewInt(3),
+		new(big.Int).Sub(bn256.Order, big.NewInt(2)),
+		new(big.Int).Sub(bn256.Order, big.NewInt(1)),
+	}
+	for _, test := range testCases {
+		inv := invert(test)
+		one := new(big.Int).Mul(test, inv)
+		one = one.Mod(one, bn256.Order)
+		require.Equal(t, big.NewInt(1), one)
+	}
+
+	for i := 0; i < 100; i++ {
+		x, err := rand.Int(rand.Reader, bn256.Order)
+		require.Nil(t, err)
+		if x.Sign() == 0 {
+			continue
+		}
+
+		inv := invert(x)
+		one := new(big.Int).Mul(x, inv)
+		one = one.Mod(one, bn256.Order)
+		require.Equal(t, big.NewInt(1), one)
+	}
+}
+
+func TestLagrangeCoefficientFactors(t *testing.T) {
+	l01 := lagrangeCoefficientFactor(0, 1)
+	l02 := lagrangeCoefficientFactor(0, 2)
+	l10 := lagrangeCoefficientFactor(1, 0)
+	l12 := lagrangeCoefficientFactor(1, 2)
+	l20 := lagrangeCoefficientFactor(2, 0)
+	l21 := lagrangeCoefficientFactor(2, 1)
+
+	qMinus1 := new(big.Int).Sub(bn256.Order, big.NewInt(1))
+	qMinus2 := new(big.Int).Sub(bn256.Order, big.NewInt(2))
+
+	l01.Mul(l01, qMinus1)
+	l01.Mod(l01, bn256.Order)
+	require.Equal(t, big.NewInt(1), l01)
+	l02.Mul(l02, qMinus2)
+	l02.Mod(l02, bn256.Order)
+	require.Equal(t, big.NewInt(1), l02)
+
+	require.Equal(t, big.NewInt(2), l10)
+	l12.Mul(l12, qMinus1)
+	l12.Mod(l12, bn256.Order)
+	require.Equal(t, big.NewInt(2), l12)
+
+	l20.Mul(l20, big.NewInt(2))
+	l20.Mod(l20, bn256.Order)
+	require.Equal(t, big.NewInt(3), l20)
+	require.Equal(t, big.NewInt(3), l21)
+}
+
+func TestLagrangeCoefficients(t *testing.T) {
+	require.Equal(t, big.NewInt(1), lagrangeCoefficient(0, []int{0}))
+	require.Equal(t, big.NewInt(1), lagrangeCoefficient(1, []int{1}))
+	require.Equal(t, big.NewInt(1), lagrangeCoefficient(2, []int{2}))
+
+	require.Equal(t, lagrangeCoefficientFactor(1, 0), lagrangeCoefficient(0, []int{0, 1}))
+	require.Equal(t, lagrangeCoefficientFactor(0, 1), lagrangeCoefficient(1, []int{0, 1}))
+
+	l0 := lagrangeCoefficient(0, []int{0, 1, 2})
+	l0Exp := lagrangeCoefficientFactor(1, 0)
+	l0Exp.Mul(l0Exp, lagrangeCoefficientFactor(2, 0))
+	l0Exp.Mod(l0Exp, bn256.Order)
+	require.Equal(t, l0Exp, l0)
+
+	l1 := lagrangeCoefficient(1, []int{0, 1, 2})
+	l1Exp := lagrangeCoefficientFactor(0, 1)
+	l1Exp.Mul(l1Exp, lagrangeCoefficientFactor(2, 1))
+	l1Exp.Mod(l1Exp, bn256.Order)
+	require.Equal(t, l1Exp, l1)
+
+	l2 := lagrangeCoefficient(2, []int{0, 1, 2})
+	l2Exp := lagrangeCoefficientFactor(0, 2)
+	l2Exp.Mul(l2Exp, lagrangeCoefficientFactor(1, 2))
+	l2Exp.Mod(l2Exp, bn256.Order)
+	require.Equal(t, l2Exp, l2)
+}
+
+func TestLagrangeReconstruct(t *testing.T) {
+	p, err := RandomPolynomial(rand.Reader, uint64(2), big.NewInt(10))
+	require.Nil(t, err)
+
+	l1 := lagrangeCoefficient(0, []int{0, 1, 2})
+	l2 := lagrangeCoefficient(1, []int{0, 1, 2})
+	l3 := lagrangeCoefficient(2, []int{0, 1, 2})
+	v1 := p.EvalForKeyper(0)
+	v2 := p.EvalForKeyper(1)
+	v3 := p.EvalForKeyper(2)
+
+	y1 := new(big.Int).Mul(l1, v1)
+	y2 := new(big.Int).Mul(l2, v2)
+	y3 := new(big.Int).Mul(l3, v3)
+	y1.Mod(y1, bn256.Order)
+	y2.Mod(y2, bn256.Order)
+	y3.Mod(y3, bn256.Order)
+
+	y := new(big.Int).Add(y1, y2)
+	y.Add(y, y3)
+	y.Mod(y, bn256.Order)
+
+	require.Equal(t, big.NewInt(10), y)
+}
+
+func TestComputeEpochSKShare(t *testing.T) {
+	eonSKShare := (*EonSKShare)(big.NewInt(123))
+	epochID := ComputeEpochID(uint64(456))
+	epochSKShare := ComputeEpochSKShare(eonSKShare, epochID)
+	expectedEpochSKShare := new(bn256.G1).ScalarMult((*bn256.G1)(epochID), (*big.Int)(eonSKShare))
+	require.Equal(t, (*bn256.G1)(epochSKShare).String(), expectedEpochSKShare.String())
+}
+
+func TestVerifyEpochSKShare(t *testing.T) {
+	threshold := uint64(2)
+	epochID := ComputeEpochID(uint64(10))
+	p1, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(10))
+	require.Nil(t, err)
+	p2, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(20))
+	require.Nil(t, err)
+	p3, err := RandomPolynomial(rand.Reader, threshold, big.NewInt(30))
+	require.Nil(t, err)
+
+	gammas := []*Gammas{
+		p1.Gammas(),
+		p2.Gammas(),
+		p3.Gammas(),
+	}
+
+	epk1 := ComputeEonPKShare(0, gammas)
+	epk2 := ComputeEonPKShare(1, gammas)
+	epk3 := ComputeEonPKShare(2, gammas)
+	esk1 := ComputeEonSKShare([]*big.Int{p1.EvalForKeyper(0), p2.EvalForKeyper(0), p3.EvalForKeyper(0)})
+	esk2 := ComputeEonSKShare([]*big.Int{p1.EvalForKeyper(1), p2.EvalForKeyper(1), p3.EvalForKeyper(1)})
+	esk3 := ComputeEonSKShare([]*big.Int{p1.EvalForKeyper(2), p2.EvalForKeyper(2), p3.EvalForKeyper(2)})
+	epsk1 := ComputeEpochSKShare(esk1, epochID)
+	epsk2 := ComputeEpochSKShare(esk2, epochID)
+	epsk3 := ComputeEpochSKShare(esk3, epochID)
+
+	require.True(t, VerifyEpochSKShare(epsk1, epk1, epochID))
+	require.True(t, VerifyEpochSKShare(epsk2, epk2, epochID))
+	require.True(t, VerifyEpochSKShare(epsk3, epk3, epochID))
+
+	require.False(t, VerifyEpochSKShare(epsk1, epk2, epochID))
+	require.False(t, VerifyEpochSKShare(epsk2, epk1, epochID))
+	require.False(t, VerifyEpochSKShare(epsk1, epk1, ComputeEpochID(uint64(11))))
+}
+
+func TestFull(t *testing.T) {
+	threshold := uint64(2)
+
+	b1, err := RandomPolynomialBase(rand.Reader)
+	require.Nil(t, err)
+	b2, err := RandomPolynomialBase(rand.Reader)
+	require.Nil(t, err)
+	b3, err := RandomPolynomialBase(rand.Reader)
+	require.Nil(t, err)
+
+	p1, err := RandomPolynomial(rand.Reader, threshold, b1)
+	require.Nil(t, err)
+	p2, err := RandomPolynomial(rand.Reader, threshold, b2)
+	require.Nil(t, err)
+	p3, err := RandomPolynomial(rand.Reader, threshold, b3)
+	require.Nil(t, err)
+
+	gamma1 := p1.Gammas()
+	gamma2 := p2.Gammas()
+	gamma3 := p3.Gammas()
+
+	v11 := p1.EvalForKeyper(0)
+	v21 := p2.EvalForKeyper(0)
+	v31 := p3.EvalForKeyper(0)
+	v12 := p1.EvalForKeyper(1)
+	v22 := p2.EvalForKeyper(1)
+	v32 := p3.EvalForKeyper(1)
+	v13 := p1.EvalForKeyper(2)
+	v23 := p2.EvalForKeyper(2)
+	v33 := p3.EvalForKeyper(2)
+
+	require.True(t, VerifyPolyEval(0, v11, gamma1, threshold))
+	require.True(t, VerifyPolyEval(0, v21, gamma2, threshold))
+	require.True(t, VerifyPolyEval(0, v31, gamma3, threshold))
+	require.True(t, VerifyPolyEval(1, v12, gamma1, threshold))
+	require.True(t, VerifyPolyEval(1, v22, gamma2, threshold))
+	require.True(t, VerifyPolyEval(1, v32, gamma3, threshold))
+	require.True(t, VerifyPolyEval(2, v13, gamma1, threshold))
+	require.True(t, VerifyPolyEval(2, v23, gamma2, threshold))
+	require.True(t, VerifyPolyEval(2, v33, gamma3, threshold))
+
+	eonSK1 := ComputeEonSKShare([]*big.Int{v11, v21, v31})
+	eonSK2 := ComputeEonSKShare([]*big.Int{v12, v22, v32})
+	eonSK3 := ComputeEonSKShare([]*big.Int{v13, v23, v33})
+
+	gammas := []*Gammas{gamma1, gamma2, gamma3}
+	eonPK1 := ComputeEonPKShare(0, gammas)
+	eonPK2 := ComputeEonPKShare(1, gammas)
+	eonPK3 := ComputeEonPKShare(2, gammas)
+
+	epochID := ComputeEpochID(123)
+	epochSK1 := ComputeEpochSKShare(eonSK1, epochID)
+	epochSK2 := ComputeEpochSKShare(eonSK2, epochID)
+	epochSK3 := ComputeEpochSKShare(eonSK3, epochID)
+
+	require.True(t, VerifyEpochSKShare(epochSK1, eonPK1, epochID))
+	require.True(t, VerifyEpochSKShare(epochSK2, eonPK2, epochID))
+	require.True(t, VerifyEpochSKShare(epochSK3, eonPK3, epochID))
+
+	// TODO: add encryption/decryption when implemented
+}
