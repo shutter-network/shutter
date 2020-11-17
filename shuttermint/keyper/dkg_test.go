9d063dd9f9d24b0c8ed6a5be117070016fef140d
Ralf Schmitt <ralf@brainbot.com>
2020-11-17 12:03:45 +0100

    Refactor keyper DKG test to allow reuse of initialization code
    Also cleanup/cancel the additional go routine started during the test.

--- shuttermint/keyper/dkg_test.go
+++ shuttermint/keyper/dkg_test.go
@@ -10,11 +10,23 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/brainbot-com/shutter/shuttermint/contract"
 )
 
-func TestDKGInstance(t *testing.T) {
+type testInstance struct {
+	eon                         uint64
+	threshold                   uint64
+	keypers                     []common.Address
+	keyperSigningKeys           map[common.Address]*ecdsa.PrivateKey
+	keyperEncryptionPrivateKeys map[common.Address]*ecies.PrivateKey
+	keyperEncryptionPublicKeys  map[common.Address]*ecies.PublicKey
+	ms                          MockMessageSender
+	dkg                         *DKGInstance
+}
+
+func setupTestInstance(t *testing.T) testInstance {
 	eon := uint64(10)
 	threshold := uint64(2)
 	n := 3
@@ -53,31 +65,53 @@ func TestDKGInstance(t *testing.T) {
 	require.Equal(t, eon, dkg.Eon)
 	require.NotNil(t, dkg.Polynomial)
 
-	go dkg.Run(context.Background())
+	return testInstance{
+		eon:                         eon,
+		threshold:                   threshold,
+		keypers:                     keypers,
+		keyperSigningKeys:           keyperSigningKeys,
+		keyperEncryptionPrivateKeys: keyperEncryptionPrivateKeys,
+		keyperEncryptionPublicKeys:  keyperEncryptionPublicKeys,
+		ms:                          ms,
+		dkg:                         dkg,
+	}
+}
+
+func TestDKGInstance(t *testing.T) {
+	ti := setupTestInstance(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	group, _ := errgroup.WithContext(ctx)
+	group.Go(func() error {
+		return ti.dkg.Run(ctx)
+	})
 
 	t.Run("SendGammas", func(t *testing.T) {
-		msgContainer := <-ms.Msgs
+		msgContainer := <-ti.ms.Msgs
 		msg := msgContainer.GetPolyCommitmentMsg()
 		require.NotNil(t, msg)
-		require.Equal(t, eon, msg.Eon)
+		require.Equal(t, ti.eon, msg.Eon)
 		gammas := [][]byte{}
-		for _, g := range *dkg.Polynomial.Gammas() {
+		for _, g := range *ti.dkg.Polynomial.Gammas() {
 			gammas = append(gammas, g.Marshal())
 		}
 		require.Equal(t, gammas, msg.Gammas)
 	})
 
 	t.Run("SendPolyEvals", func(t *testing.T) {
-		for i, receiver := range keypers {
-			msgContainer := <-ms.Msgs
+		for i, receiver := range ti.keypers {
+			msgContainer := <-ti.ms.Msgs
 			msg := msgContainer.GetPolyEvalMsg()
 			require.NotNil(t, msg)
-			require.Equal(t, eon, msg.Eon)
+			require.Equal(t, ti.eon, msg.Eon)
 			require.Equal(t, receiver.Bytes(), msg.Receiver)
-			polyEval := dkg.Polynomial.EvalForKeyper(i)
-			decryptedEval, err := keyperEncryptionPrivateKeys[receiver].Decrypt(msg.EncryptedEval, nil, nil)
+			polyEval := ti.dkg.Polynomial.EvalForKeyper(i)
+			decryptedEval, err := ti.keyperEncryptionPrivateKeys[receiver].Decrypt(msg.EncryptedEval, nil, nil)
 			require.Nil(t, err)
 			require.True(t, bytes.Equal(polyEval.Bytes(), decryptedEval))
 		}
 	})
+
+	err := group.Wait()
+	require.Nil(t, err)
 }
