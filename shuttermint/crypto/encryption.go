f7060f5e25484e441c020b29752d5e97cba8314e
Jannik Luhn <jannik@brainbot.com>
2020-11-20 20:45:07 +0100

    Implement encryption and decryption functions

--- shuttermint/crypto/encryption.go
+++ shuttermint/crypto/encryption.go
@@ -1,9 +1,10 @@
 package crypto
 
 import (
-	"crypto/rand"
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"io"
-	"log"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -14,62 +15,182 @@ import (
 // SymmetricKey is the key used to encrypt and decrypt messages.
 type SymmetricKey common.Hash
 
-// EncryptionResult represents the full output of the encryption procedure.
-type EncryptionResult struct {
-	RandomnessWitness *bn256.G2
-	CipherText        []byte
+// EncryptedMessage represents the full output of the encryption procedure.
+type EncryptedMessage struct {
+	C1 *bn256.G2
+	C2 Block
+	C3 []Block
 }
 
-// RandomR returns a random value to be used during encryption.
-func RandomR(r io.Reader) (*big.Int, error) {
-	return rand.Int(r, bn256.Order)
+// Block represents a block of data.
+type Block [BlockSize]byte
+
+// BlockSize is the size in bytes of the blocks into which a message is split up before encryption.
+const BlockSize = 32
+
+// HashBytesToBlock hashes the given byte slice and returns the result as a block.
+func HashBytesToBlock(d []byte) Block {
+	h := crypto.Keccak256(d)
+	var b Block
+	copy(b[:], h)
+	return b
 }
 
-func computeEncryptionKey(r *big.Int, eonPK *EonPK, epochID *EpochID) *SymmetricKey {
-	g := bn256.Pair((*bn256.G1)(epochID), (*bn256.G2)(eonPK))
-	g = new(bn256.GT).ScalarMult(g, r)
-	log.Println("enc")
-	log.Println(g.String())
-	gBytes := g.Marshal()
-	h := crypto.Keccak256Hash(gBytes)
-	k := SymmetricKey(h)
-	return &k
+// HashBlockToInt hashes a block and returns the result as an integer in Z_q.
+func HashBlockToInt(d Block) *big.Int {
+	h := crypto.Keccak256(d[:])
+	i := new(big.Int).SetBytes(h)
+	i.Mod(i, bn256.Order) // TODO: check if this is fine
+	return i
 }
 
-func computeCipherText(message []byte, key *SymmetricKey) []byte {
-	// TODO: encryption
-	return message
+// HashGTToBlock hashes an element of GT and returns the result as a block.
+func HashGTToBlock(gt *bn256.GT) Block {
+	b := gt.Marshal()
+	return HashBytesToBlock(b)
 }
 
-// Encrypt encrypts a message for a given epoch using the provided eon key and randomness.
-func Encrypt(message []byte, r *big.Int, eonPK *EonPK, epochID *EpochID) *EncryptionResult {
-	key := computeEncryptionKey(r, eonPK, epochID)
-	cipherText := computeCipherText(message, key)
-	c := EncryptionResult{
-		RandomnessWitness: new(bn256.G2).ScalarBaseMult(r),
-		CipherText:        cipherText,
+// XORBlocks xors the two blocks and returns the result.
+func XORBlocks(b1 Block, b2 Block) Block {
+	var b Block
+	for i := 0; i < BlockSize; i++ {
+		b[i] = b1[i] ^ b2[i]
 	}
-	return &c
+	return b
 }
 
-func computeDecryptionKey(epochSK *EpochSK, c *EncryptionResult) *SymmetricKey {
-	g := bn256.Pair((*bn256.G1)(epochSK), c.RandomnessWitness)
-	log.Println("dec")
-	log.Println(g.String())
-	gBytes := g.Marshal()
-	h := crypto.Keccak256Hash(gBytes)
-	k := SymmetricKey(h)
-	return &k
+// RandomSigma returns a random value to be used during encryption.
+func RandomSigma(r io.Reader) (Block, error) {
+	data := make([]byte, BlockSize)
+	_, err := r.Read(data)
+	if err != nil {
+		return Block{}, nil
+	}
+	var b Block
+	copy(b[:], data)
+	return b, nil
 }
 
-// Decrypt decrypts an encrypted message using the given epoch secret key.
-func Decrypt(c *EncryptionResult, epochSK *EpochSK) []byte {
-	key := computeDecryptionKey(epochSK, c)
-	p := computePlainText(c.CipherText, key)
-	return p
+// Encrypt encrypts a message for the epoch given by its id. It uses the eon pk and randomness
+// provided in sigma.
+func Encrypt(message []byte, eonPK *EonPK, epochID *EpochID, sigma Block) *EncryptedMessage {
+	messageBlocks := PadMessage(message)
+	r := computeR(sigma)
+	result := EncryptedMessage{
+		C1: computeC1(r),
+		C2: computeC2(sigma, r, epochID, eonPK),
+		C3: computeC3(messageBlocks, sigma),
+	}
+	return &result
 }
 
-func computePlainText(cipherText []byte, key *SymmetricKey) []byte {
-	// TODO: decryption
-	return cipherText
+func computeR(sigma Block) *big.Int {
+	return HashBlockToInt(sigma)
+}
+
+func computeC1(r *big.Int) *bn256.G2 {
+	return new(bn256.G2).ScalarBaseMult(r)
+}
+
+func computeC2(sigma Block, r *big.Int, epochID *EpochID, eonPK *EonPK) Block {
+	pairing := bn256.Pair((*bn256.G1)(epochID), (*bn256.G2)(eonPK))
+	preimage := new(bn256.GT).ScalarMult(pairing, r)
+	key := HashGTToBlock(preimage)
+	return XORBlocks(sigma, key)
+}
+
+func computeC3(blocks []Block, sigma Block) []Block {
+	encryptedBlocks := []Block{}
+	numBlocks := len(blocks)
+	keys := computeBlockKeys(sigma, numBlocks)
+	for i := 0; i < numBlocks; i++ {
+		encryptedBlock := XORBlocks(keys[i], blocks[i])
+		encryptedBlocks = append(encryptedBlocks, encryptedBlock)
+	}
+	return encryptedBlocks
+}
+
+func computeBlockKeys(sigma Block, n int) []Block {
+	keys := []Block{}
+	buf := make([]byte, binary.MaxVarintLen64)
+	for i := int64(0); i < int64(n); i++ {
+		binary.PutVarint(buf, i)
+		preimage := append(sigma[:], buf...)
+		key := HashBytesToBlock(preimage)
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+// Decrypt decrypts the given message using the given epoch secret key.
+func (m *EncryptedMessage) Decrypt(epochSK *EpochSK) ([]byte, error) {
+	sigma := m.Sigma(epochSK)
+	decryptedBlocks := decryptBlocks(m.C3, sigma)
+	return UnpadMessage(decryptedBlocks)
+}
+
+// Sigma computes the sigma value of the encrypted message given the epoch secret key.
+func (m *EncryptedMessage) Sigma(epochSK *EpochSK) Block {
+	pairing := bn256.Pair((*bn256.G1)(epochSK), m.C1)
+	key := HashGTToBlock(pairing)
+	sigma := XORBlocks(m.C2, key)
+	return sigma
+}
+
+func decryptBlocks(encryptedBlocks []Block, sigma Block) []Block {
+	numBlocks := len(encryptedBlocks)
+	keys := computeBlockKeys(sigma, numBlocks)
+	decryptedBlocks := []Block{}
+	for i := 0; i < numBlocks; i++ {
+		decryptedBlock := XORBlocks(encryptedBlocks[i], keys[i])
+		decryptedBlocks = append(decryptedBlocks, decryptedBlock)
+	}
+	return decryptedBlocks
+}
+
+// PadMessage pads a message and returns it as a sequence of blocks.
+// Implements PKCS #7 according to https://www.ietf.org/rfc/rfc2315.txt
+func PadMessage(m []byte) []Block {
+	paddingLength := BlockSize - len(m)%BlockSize
+	padding := bytes.Repeat([]byte{byte(paddingLength)}, paddingLength)
+	padded := append(m, padding...)
+
+	blocks := []Block{}
+	numBlocks := len(padded) / BlockSize
+	for i := 0; i < numBlocks; i++ {
+		var block Block
+		copy(block[:], padded[i*BlockSize:(i+1)*BlockSize])
+		blocks = append(blocks, block)
+	}
+	return blocks
+}
+
+// UnpadMessage returns the message provided in padded form as a sequence of blocks.
+func UnpadMessage(blocks []Block) ([]byte, error) {
+	if len(blocks) == 0 {
+		return []byte{}, nil
+	}
+
+	lastBlock := blocks[len(blocks)-1]
+	paddingLength := int(lastBlock[BlockSize-1])
+	if paddingLength == 0 {
+		return nil, fmt.Errorf("invalid padding length 0")
+	}
+	if paddingLength > BlockSize {
+		return nil, fmt.Errorf("invalid padding length %d (greater than block size %d)", paddingLength, BlockSize)
+	}
+
+	m := make([]byte, len(blocks)*BlockSize-paddingLength)
+	// copy unpadded blocks
+	for i := 0; i < len(blocks)-1; i++ {
+		start := i * BlockSize
+		end := (i + 1) * BlockSize
+		copy(m[start:end], blocks[i][:])
+	}
+	// copy padded block
+	start := (len(blocks) - 1) * BlockSize
+	end := len(blocks)*BlockSize - paddingLength
+	copy(m[start:end], lastBlock[:BlockSize-paddingLength])
+
+	return m, nil
 }
--- shuttermint/crypto/encryption_test.go
+++ shuttermint/crypto/encryption_test.go
@@ -3,12 +3,201 @@ package crypto
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/hex"
 	"math/big"
+	"strings"
 	"testing"
 
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/stretchr/testify/require"
 )
+
+func TestRandomSigma(t *testing.T) {
+	firstByteAlways0 := true
+	lastByteAlways0 := true
+	for i := 0; i < 10; i++ {
+		sigma, err := RandomSigma(rand.Reader)
+		require.Nil(t, err)
+		if sigma[0] != 0 {
+			firstByteAlways0 = false
+		}
+		if sigma[BlockSize-1] != 0 {
+			lastByteAlways0 = false
+		}
+	}
+	require.False(t, firstByteAlways0)
+	require.False(t, lastByteAlways0)
+}
+
+func TestPadding(t *testing.T) {
+	testCases := []struct {
+		mHex  string
+		bsHex []string
+	}{
+		{
+			"",
+			[]string{
+				strings.Repeat("20", 32),
+			},
+		},
+		{
+			"99",
+			[]string{
+				"99" + strings.Repeat("1f", 31),
+			},
+		},
+		{
+			"9999",
+			[]string{
+				"9999" + strings.Repeat("1e", 30),
+			},
+		},
+		{
+			strings.Repeat("99", 31),
+			[]string{
+				strings.Repeat("99", 31) + "01",
+			},
+		},
+		{
+			strings.Repeat("99", 32),
+			[]string{
+				strings.Repeat("99", 32),
+				strings.Repeat("20", 32),
+			},
+		},
+		{
+			strings.Repeat("99", 33),
+			[]string{
+				strings.Repeat("99", 32),
+				"99" + strings.Repeat("1f", 31),
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		m, err := hex.DecodeString(test.mHex)
+		require.Nil(t, err)
+		bs := []Block{}
+		for _, bHex := range test.bsHex {
+			bBytes, err := hex.DecodeString(bHex)
+			require.Nil(t, err)
+			require.Equal(t, 32, len(bBytes))
+			var bByteArray [32]byte
+			copy(bByteArray[:], bBytes)
+			b := Block(bByteArray)
+			bs = append(bs, b)
+		}
+
+		padded := PadMessage(m)
+		require.Equal(t, bs, padded)
+	}
+}
+
+func TestUnpadding(t *testing.T) {
+	invalidBs := [][]string{
+		[]string{
+			strings.Repeat("00", 32),
+		},
+		[]string{
+			"aabbcc" + strings.Repeat("00", 29),
+		},
+		[]string{
+			strings.Repeat("21", 32),
+		},
+		[]string{
+			strings.Repeat("99", 32),
+			strings.Repeat("00", 32),
+		},
+	}
+	for _, bsHex := range invalidBs {
+		bs := []Block{}
+		for _, bHex := range bsHex {
+			bBytes, err := hex.DecodeString(bHex)
+			require.Nil(t, err)
+			require.Equal(t, 32, len(bBytes))
+			var bByteArray [32]byte
+			copy(bByteArray[:], bBytes)
+			b := Block(bByteArray)
+			bs = append(bs, b)
+		}
+
+		_, err := UnpadMessage(bs)
+		require.NotNil(t, err)
+	}
+
+	testCases := []struct {
+		bsHex []string
+		mHex  string
+	}{
+		{
+			[]string{
+				strings.Repeat("20", 32),
+			},
+			"",
+		},
+		{
+			[]string{
+				"99" + strings.Repeat("1f", 31),
+			},
+			"99",
+		},
+		{
+			[]string{
+				strings.Repeat("99", 31) + "01",
+			},
+			strings.Repeat("99", 31),
+		},
+		{
+			[]string{
+				strings.Repeat("99", 32),
+				strings.Repeat("20", 32),
+			},
+			strings.Repeat("99", 32),
+		},
+	}
+	for _, test := range testCases {
+		bs := []Block{}
+		for _, bHex := range test.bsHex {
+			bBytes, err := hex.DecodeString(bHex)
+			require.Nil(t, err)
+			require.Equal(t, 32, len(bBytes))
+			var bByteArray [32]byte
+			copy(bByteArray[:], bBytes)
+			b := Block(bByteArray)
+			bs = append(bs, b)
+		}
+		m, err := hex.DecodeString(test.mHex)
+		require.Nil(t, err)
+
+		unpadded, err := UnpadMessage(bs)
+		require.Nil(t, err)
+		require.True(t, bytes.Equal(m, unpadded))
+	}
+}
+
+func TestPaddingRoundtrip(t *testing.T) {
+	ms := [][]byte{
+		[]byte(""),
+		[]byte("a"),
+		bytes.Repeat([]byte("a"), 31),
+		bytes.Repeat([]byte("a"), 32),
+		bytes.Repeat([]byte("a"), 33),
+	}
+	for i := 0; i < 100; i++ {
+		l, err := rand.Int(rand.Reader, big.NewInt(100))
+		require.Nil(t, err)
+		m := make([]byte, l.Int64())
+		_, err = rand.Read(m)
+		require.Nil(t, err)
+		ms = append(ms, m)
+	}
+	for _, m := range ms {
+		padded := PadMessage(m)
+		unpadded, err := UnpadMessage(padded)
+		require.Nil(t, err)
+		require.True(t, bytes.Equal(m, unpadded))
+	}
+}
 
 func TestRoundTrip(t *testing.T) {
 	// first generate keys
@@ -49,17 +238,11 @@ func TestRoundTrip(t *testing.T) {
 
 	// now encrypt and decrypt message
 	m := []byte("hello")
-	r, err := RandomR(rand.Reader)
+	sigma, err := RandomSigma(rand.Reader)
 	require.Nil(t, err)
 
-	c := Encrypt(m, r, eonPK, epochID)
-	c1Exp := new(bn256.G2).ScalarBaseMult(r)
-	require.True(t, EqualG2(c.RandomnessWitness, c1Exp))
-
-	encKey := computeEncryptionKey(r, eonPK, epochID)
-	decKey := computeDecryptionKey(epochSK, c)
-	require.Equal(t, encKey, decKey)
-
-	m2 := Decrypt(c, epochSK)
-	require.True(t, bytes.Equal(m, m2))
+	encM := Encrypt(m, eonPK, epochID, sigma)
+	decM, err := encM.Decrypt(epochSK)
+	require.Nil(t, err)
+	require.True(t, bytes.Equal(m, decM))
 }
