52570408163ccce805fffb940c4dcbdff069ddf2
Ralf Schmitt <ralf@brainbot.com>
2020-12-18 12:43:06 +0100

    Cleanup shutterevents tests

--- shuttermint/keyper/shutterevents/events_test.go
+++ shuttermint/keyper/shutterevents/events_test.go
@@ -38,6 +38,14 @@ func init() {
 	gammas = *polynomial.Gammas()
 }
 
+// roundtrip checks that the given IEvent round-trips, i.e. it can be serialized as an ABCI Event
+// and deserialized back again to an equal value.
+func roundtrip(t *testing.T, ev shutterevents.IEvent) {
+	ev2, err := shutterevents.MakeEvent(ev.MakeABCIEvent())
+	require.Nil(t, err)
+	require.Equal(t, ev, ev2)
+}
+
 func TestAccusation(t *testing.T) {
 	ev := shutterevents.Accusation{
 		Eon:     eon,
@@ -63,7 +71,7 @@ func TestApology(t *testing.T) {
 	roundtrip(t, ev)
 }
 
-func TestMakeEventBatchConfig(t *testing.T) {
+func TestBatchConfig(t *testing.T) {
 	ev := shutterevents.BatchConfig{
 		StartBatchIndex: 111,
 		Threshold:       2,
@@ -73,52 +81,11 @@ func TestMakeEventBatchConfig(t *testing.T) {
 	roundtrip(t, ev)
 }
 
-// roundtrip checks that the given IEvent round-trips, i.e. it can be serialized as an ABCI Event
-// and deserialized back again to an equal value.
-func roundtrip(t *testing.T, ev shutterevents.IEvent) {
-	ev2, err := shutterevents.MakeEvent(ev.MakeABCIEvent())
-	require.Nil(t, err)
-	require.Equal(t, ev, ev2)
-}
-
-func TestCheckInEvent(t *testing.T) {
+func TestCheckIn(t *testing.T) {
 	privateKeyECDSA, err := ethcrypto.GenerateKey()
 	require.Nil(t, err)
 	publicKey := ecies.ImportECDSAPublic(&privateKeyECDSA.PublicKey)
-
 	ev := shutterevents.CheckIn{Sender: sender, EncryptionPublicKey: publicKey}
-	roundtrip(t, ev)
-}
-
-func TestMakeEonStartedEvent(t *testing.T) {
-	ev := shutterevents.EonStarted{Eon: eon, BatchIndex: 9999}
-	roundtrip(t, ev)
-}
-
-func TestMakePolyCommitmentRegisteredEvent(t *testing.T) {
-	ev := shutterevents.PolyCommitment{
-		Eon:    eon,
-		Sender: sender,
-		Gammas: &gammas,
-	}
-	roundtrip(t, ev)
-}
-
-func TestPolyEval(t *testing.T) {
-	var receivers []common.Address
-	var encryptedEvals [][]byte
-
-	for i := 1; i < 10; i++ {
-		receivers = append(receivers, common.BigToAddress(new(big.Int).SetUint64(uint64(i))))
-		encryptedEvals = append(encryptedEvals, []byte(fmt.Sprintf("encrypted: %d", i)))
-	}
-
-	ev := shutterevents.PolyEval{
-		Eon:            eon,
-		Sender:         sender,
-		Receivers:      receivers,
-		EncryptedEvals: encryptedEvals,
-	}
 	roundtrip(t, ev)
 }
 
@@ -131,31 +98,32 @@ func TestDecryptionSignature(t *testing.T) {
 	roundtrip(t, ev)
 }
 
-// // gammasToMsg converts the gammas to what the keyper sends to shuttermint
-// func gammasToMsg(gammas crypto.Gammas) [][]byte {
-//	// original implementation in NewPolyCommitmentMsg
-//	gammaBytes := [][]byte{}
-//	for _, gamma := range gammas {
-//		gammaBytes = append(gammaBytes, gamma.Marshal())
-//	}
-//	return gammaBytes
-// }
+func TestEonStarted(t *testing.T) {
+	ev := shutterevents.EonStarted{Eon: eon, BatchIndex: 9999}
+	roundtrip(t, ev)
+}
 
-// // gammasToEvent converts the gammas to what we get in a shuttermint event
-// func gammasToEvent(gammas crypto.Gammas) []byte {
-//	data := gammasToMsg(gammas) // this is what the keyper sends to shuttermint
+func TestPolyCommitment(t *testing.T) {
+	ev := shutterevents.PolyCommitment{
+		Eon:    eon,
+		Sender: sender,
+		Gammas: &gammas,
+	}
+	roundtrip(t, ev)
+}
 
-//	// Convert it to event data like newGammas defined in app/events.go
-//	var encoded []string
-//	for _, i := range data {
-//		encoded = append(encoded, hex.EncodeToString(i))
-//	}
-//	return []byte(strings.Join(encoded, ","))
-// }
-
-// func TestDecodeGammasFromEvent(t *testing.T) {
-//	eventValue := gammasToEvent(gammas)
-//	decoded, err := decodeGammas(eventValue)
-//	require.Nil(t, err)
-//	require.Equal(t, gammas, decoded)
-// }
+func TestPolyEval(t *testing.T) {
+	var receivers []common.Address
+	var encryptedEvals [][]byte
+	for i := 1; i < 10; i++ {
+		receivers = append(receivers, common.BigToAddress(new(big.Int).SetUint64(uint64(i))))
+		encryptedEvals = append(encryptedEvals, []byte(fmt.Sprintf("encrypted: %d", i)))
+	}
+	ev := shutterevents.PolyEval{
+		Eon:            eon,
+		Sender:         sender,
+		Receivers:      receivers,
+		EncryptedEvals: encryptedEvals,
+	}
+	roundtrip(t, ev)
+}
