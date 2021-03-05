fd38c5154107ce9a64a9885c5bbd6ca79d65179e
Ralf Schmitt <ralf@brainbot.com>
2021-03-05 18:45:59 +0100

    Do not use varints for the shuffle code

--- shuttermint/shcrypto/shuffle.go
+++ shuttermint/shcrypto/shuffle.go
@@ -8,24 +8,23 @@ import (
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 )
 
+// computeSeed computes a seed value from the EpochSecretKey
+func computeSeed(key *EpochSecretKey) int64 {
+	keyBytes := (*bn256.G1)(key).Marshal()
+	keyHash := crypto.Keccak256(keyBytes)
+	return int64(binary.LittleEndian.Uint64(keyHash[:8]))
+}
+
 // Shuffle shuffles the order of the transactions using the epoch secret key as a source of
 // randomness.
 func Shuffle(txs [][]byte, key *EpochSecretKey) [][]byte {
-	keyBytes := (*bn256.G1)(key).Marshal()
-	keyHash := crypto.Keccak256(keyBytes)
-
-	seed, n := binary.Varint(keyHash[:8])
-	if n <= 0 {
-		panic("failed to decode hash as int64")
-	}
-	source := rand.NewSource(seed)
-	rand := rand.New(source)
-
 	shuffledTxs := append([][]byte{}, txs...)
 	swap := func(i, j int) {
 		shuffledTxs[i], shuffledTxs[j] = shuffledTxs[j], shuffledTxs[i]
 	}
-	rand.Shuffle(len(shuffledTxs), swap)
-
+	// XXX Using go's stdlib for shuffling may mean the result of this function changes when
+	// using a different go version
+	r := rand.New(rand.NewSource(computeSeed(key)))
+	r.Shuffle(len(shuffledTxs), swap)
 	return shuffledTxs
 }
