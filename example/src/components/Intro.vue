a33b3b587a9fa5b84bac9630d11cf09e07226fe5
Jannik Luhn <jannik@brainbot.com>
2021-04-08 14:24:27 +0200

    Update example application intro

--- example/src/components/Intro.vue
+++ example/src/components/Intro.vue
@@ -2,8 +2,8 @@
   <div class="columns">
     <div class="column is-offset-2 is-two-thirds">
       <p>
-        This minimal example application illustrates the user experience of
-        Shutter. It is the first frontrunning resistant public on-chain
+        This is a minimal example application using Shutter on the Goerli
+        testnet. It is the first frontrunning resistant public on-chain
         billboard! For more information, check out
         <a
           href="https://shutter.ghost.io/p/711def71-d5f5-4786-9c46-525966aade56/"
