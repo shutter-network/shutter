bf2e86d872b9c22aefe934955eed5a2c0544eb89
Ralf Schmitt <ralf@brainbot.com>
2021-05-07 09:11:21 +0200

    Remove the unused sandbox.ExitIfError
    I think we should accept and live with go's error handling. If we
    handle the error at the outer function, it just saves 3 lines of code
    to call the equivalent of ExitIfError once. We've replaced this with
    using cobra's RunE function instead.

--- shuttermint/sandbox/utils.go
+++ shuttermint/sandbox/utils.go
@@ -3,22 +3,12 @@ package sandbox
 import (
 	"context"
 	"crypto/ecdsa"
-	"fmt"
 	"math/big"
-	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
-
-// ExitIfError exits the program with an error message and status if the given error is non-nil.
-func ExitIfError(err error) {
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
 
 // InitTransactOpts initializes the transact options struct.
 func InitTransactOpts(ctx context.Context, client *ethclient.Client, key *ecdsa.PrivateKey) (*bind.TransactOpts, error) {
