dab151f8b4fb8790faad41152d96bf3332240459
Jannik Luhn <jannik@brainbot.com>
2021-06-15 14:43:58 +0200

    Disable prettier for fallback function
    It wants to remove the argument and return value.

--- contracts/contracts/TestProxyReceiver.sol
+++ contracts/contracts/TestProxyReceiver.sol
@@ -5,6 +5,7 @@ pragma solidity =0.8.4;
 contract TestProxyReceiver {
     event Called(bytes data);
 
+    // prettier-ignore
     fallback(bytes calldata data) external returns (bytes memory) {
         if (data.length > 0 && data[0] == 0) {
             require(false, "TestTargetContract: failing");
