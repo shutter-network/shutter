3607b871005ddc976d945fe80c47945dc6f9c9fe
Jannik Luhn <jannik@brainbot.com>
2021-06-15 14:17:53 +0200

    Fix fallback function signature

--- contracts/contracts/TestProxyReceiver.sol
+++ contracts/contracts/TestProxyReceiver.sol
@@ -5,7 +5,7 @@ pragma solidity =0.8.4;
 contract TestProxyReceiver {
     event Called(bytes data);
 
-    fallback() external {
+    fallback(bytes calldata data) external returns (bytes memory) {
         if (data.length > 0 && data[0] == 0) {
             require(false, "TestTargetContract: failing");
         } else {
