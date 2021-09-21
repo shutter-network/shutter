cf22a101527f534318af2e54e48883df734879d1
Jannik Luhn <jannik@brainbot.com>
2021-09-21 19:00:09 +0200

    Add methods to marshal and unmarshal bls types

--- shlib/shcrypto/encoding.go
+++ shlib/shcrypto/encoding.go
@@ -2,6 +2,7 @@ package shcrypto
 
 import (
 	"bytes"
+	"math/big"
 
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 	"github.com/pkg/errors"
@@ -59,4 +60,62 @@ func (eonpubkey *EonPublicKey) Marshal() []byte {
 func (eonpubkey *EonPublicKey) Unmarshal(m []byte) error {
 	_, err := (*bn256.G2)(eonpubkey).Unmarshal(m)
 	return err
+}
+
+// Marshal converts a BLS secret key to bytes. The result is a 32 byte slice. Make sure the input
+// is a valid key, otherwise the method might panic or return something that when decoded would not
+// match the original value.
+func (blsSecretKey *BLSSecretKey) Marshal() []byte {
+	b := make([]byte, 32)
+	(*big.Int)(blsSecretKey).FillBytes(b)
+	return b
+}
+
+// Unmarshal sets the secret key to the value stored in the given byte slice. The input byte slice
+// must be 32 bytes and contain a valid key, otherwise an error is returned.
+func (blsSecretKey *BLSSecretKey) Unmarshal(b []byte) error {
+	if len(b) != 32 {
+		return errors.Errorf("secret key must be 32 bytes, got %d", len(b))
+	}
+
+	secretKeyInt := (*big.Int)(blsSecretKey)
+	secretKeyInt.SetBytes(b)
+	if secretKeyInt.Cmp(bn256.Order) > -1 {
+		secretKeyInt.SetUint64(0) // set value to zero to avoid using a corrupt one
+		return errors.New("secret key must be smaller than the order")
+	}
+
+	return nil
+}
+
+// Marshal converts a BLS public key to bytes.
+func (blsPublicKey *BLSPublicKey) Marshal() []byte {
+	return (*bn256.G1)(blsPublicKey).Marshal()
+}
+
+// Unmarshal sets the public key to the value stored in the given byte slice. The input byte slice
+// must be 64 bytes and contain a valid key, otherwise an error is returned.
+func (blsPublicKey *BLSPublicKey) Unmarshal(b []byte) error {
+	if len(b) != 64 {
+		return errors.Errorf("public key must be 64 bytes, got %d", len(b))
+	}
+	publicKeyG1 := (*bn256.G1)(blsPublicKey)
+	_, err := publicKeyG1.Unmarshal(b)
+	return errors.Wrap(err, "public key invalid")
+}
+
+// Marshal converts a BLS signature to bytes.
+func (blsSignature *BLSSignature) Marshal() []byte {
+	return (*bn256.G2)(blsSignature).Marshal()
+}
+
+// Unmarshal sets the signature to the value stored in the given byte slice. The input byte slice
+// must be 128 bytes and contain a valid key, otherwise an error is returned.
+func (blsSignature *BLSSignature) Unmarshal(b []byte) error {
+	if len(b) != 128 {
+		return errors.Errorf("signature must be 128 bytes, got %d", len(b))
+	}
+	signatureG2 := (*bn256.G2)(blsSignature)
+	_, err := signatureG2.Unmarshal(b)
+	return errors.Wrap(err, "signature invalid")
 }
--- shlib/shcrypto/encoding_test.go
+++ shlib/shcrypto/encoding_test.go
@@ -2,6 +2,7 @@ package shcrypto
 
 import (
 	"bytes"
+	"crypto/rand"
 	"math/big"
 	"testing"
 
@@ -48,4 +49,99 @@ func TestUnmarshalBroken(t *testing.T) {
 
 	err = m.Unmarshal(d[:len(d)-1])
 	assert.Assert(t, err != nil)
+}
+
+func TestMarshalUnmarshalBLSSecretKey(t *testing.T) {
+	secretKey, _, err := RandomBLSKeyPair(rand.Reader)
+	assert.NilError(t, err)
+
+	marshaled := secretKey.Marshal()
+	assert.Check(t, len(marshaled) == 32)
+
+	unmarshaled := new(BLSSecretKey)
+	err = unmarshaled.Unmarshal(marshaled)
+	assert.NilError(t, err)
+
+	assert.Check(t, unmarshaled.Equal(secretKey))
+}
+
+func TestUnmarshalInvalidBLSSecretKey(t *testing.T) {
+	m1 := make([]byte, 32)
+	bn256.Order.FillBytes(m1)
+	sk1 := new(BLSSecretKey)
+	err := sk1.Unmarshal(m1)
+	assert.Error(t, err, "secret key must be smaller than the order")
+
+	m2 := make([]byte, 31)
+	sk2 := new(BLSSecretKey)
+	err = sk2.Unmarshal(m2)
+	assert.Error(t, err, "secret key must be 32 bytes, got 31")
+
+	m3 := make([]byte, 33)
+	sk3 := new(BLSSecretKey)
+	err = sk3.Unmarshal(m3)
+	assert.Error(t, err, "secret key must be 32 bytes, got 33")
+}
+
+func TestMarshalUnmarshalBLSPublicKey(t *testing.T) {
+	_, publicKey, err := RandomBLSKeyPair(rand.Reader)
+	assert.NilError(t, err)
+
+	marshaled := publicKey.Marshal()
+	assert.Check(t, len(marshaled) == 64)
+
+	unmarshaled := new(BLSPublicKey)
+	err = unmarshaled.Unmarshal(marshaled)
+	assert.NilError(t, err)
+
+	assert.Check(t, unmarshaled.Equal(publicKey))
+}
+
+func TestUnmarshalInvalidBLSPublicKey(t *testing.T) {
+	m1 := bytes.Repeat([]byte{255}, 64)
+	pk1 := new(BLSPublicKey)
+	err := pk1.Unmarshal(m1)
+	assert.ErrorContains(t, err, "public key invalid")
+
+	m2 := make([]byte, 63)
+	pk2 := new(BLSPublicKey)
+	err = pk2.Unmarshal(m2)
+	assert.Error(t, err, "public key must be 64 bytes, got 63")
+
+	m3 := make([]byte, 65)
+	pk3 := new(BLSPublicKey)
+	err = pk3.Unmarshal(m3)
+	assert.Error(t, err, "public key must be 64 bytes, got 65")
+}
+
+func TestMarshalUnmarshalBLSSignature(t *testing.T) {
+	secretKey, _, err := RandomBLSKeyPair(rand.Reader)
+	assert.NilError(t, err)
+	sig := BLSSign([]byte("hello"), secretKey)
+
+	marshaled := sig.Marshal()
+	assert.Check(t, len(marshaled) == 128)
+
+	unmarshaled := new(BLSSignature)
+	err = unmarshaled.Unmarshal(marshaled)
+	assert.NilError(t, err)
+
+	assert.Check(t, unmarshaled.Equal(sig))
+}
+
+func TestUnmarshalInvalidBLSSignature(t *testing.T) {
+	m1 := bytes.Repeat([]byte{255}, 128)
+	sig1 := new(BLSSignature)
+	err := sig1.Unmarshal(m1)
+	assert.ErrorContains(t, err, "signature invalid")
+
+	m2 := make([]byte, 127)
+	sig2 := new(BLSSignature)
+	err = sig2.Unmarshal(m2)
+	assert.Error(t, err, "signature must be 128 bytes, got 127")
+
+	m3 := make([]byte, 129)
+	sig3 := new(BLSSignature)
+	err = sig3.Unmarshal(m3)
+	assert.Error(t, err, "signature must be 128 bytes, got 129")
 }
