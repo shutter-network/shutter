0083d8fee439c0d00ed4cb3680d49e81f0526987
Jannik Luhn <jannik.luhn@posteo.de>
2024-04-22 23:36:12 +0200

    Update version prefix

--- shlib/shcrypto/version.go
+++ shlib/shcrypto/version.go
@@ -1,8 +1,8 @@
 package shcrypto
 
-const VersionIdentifier byte = 0x01
+const VersionIdentifier byte = 0x02
 
-// IdentifyVersion reads the version identifier byte from the given (marshalled) EncryptedMessage.
+// IdentifyVersion reads the version identifier byte from the given (marshaled) EncryptedMessage.
 func IdentifyVersion(d []byte) byte {
 	if len(d)%BlockSize == 0 {
 		return 0x00
